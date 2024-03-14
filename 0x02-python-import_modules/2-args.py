0531d83365395d7203f056ae193803efa8de090a
tbshittu <tbshittu@gmail.com>
2024-03-14 15:53:47 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-import sys
-
-def print_arguments():
-    num_arguments = len(sys.argv) - 1
-    if num_arguments == 0:
-        print("0 arguments.")
-    else:
-        print("{} argument:".format(num_arguments))
-        for i in range(1, num_arguments + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+    import sys
+    def print_arguments():
+        num_arguments = len(sys.argv) - 1
+        if num_arguments == 0:
+            print("0 arguments.")
+        else:
+            print("{} argument:".format(num_arguments))
+            for i in range(1, num_arguments + 1):
+                print("{}: {}".format(i, sys.argv[i]))
+    print(print_arguments())
