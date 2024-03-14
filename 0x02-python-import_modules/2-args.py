ebbbcb1b11e2d30f1bc8c46c6229df04256d8745
tbshittu <tbshittu@gmail.com>
2024-03-14 05:15:00 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+def print_arguments():
+    num_arguments = len(sys.argv) - 1
+    if num_arguments == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument:".format(num_arguments))
+        for i in range(1, num_arguments + 1):
+            print("{}: {}".format(i, sys.argv[i]))
+
+if __name__ == "__main__":
+    print_arguments()
